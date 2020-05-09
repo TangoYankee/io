30840ce2b74ab573538b87c6626cfc6e865f487d
tangoyankee <miller.tim108@gmail.com>
2020-05-08 17:48:58 -0700

    build

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,11 +3,7 @@ import 'components/Hero/index.css'
 import { HeroLink, heroLinks } from 'components/Hero/HeroLink'
 import * as portrait from 'img/portrait.jpg'
 
-<<<<<<< HEAD
-export function Hero() {
-=======
 export function Hero () {
->>>>>>> e5298ad76bcb9abeeefb3496afcd18d257277680
   return (
     <div className="hero">
       <HeroImg />
@@ -16,22 +12,13 @@ export function Hero () {
   )
 }
 
-<<<<<<< HEAD
-function HeroImg() {
-=======
 function HeroImg () {
->>>>>>> e5298ad76bcb9abeeefb3496afcd18d257277680
   return (
     <img id="portrait" src={portrait} title="Timothy Miller" alt="Portrait of Timothy Miller" />
   )
 }
 
-<<<<<<< HEAD
-
-function HeroLinks() {
-=======
 function HeroLinks () {
->>>>>>> e5298ad76bcb9abeeefb3496afcd18d257277680
   const heroItems = heroLinks.map((heroItem) =>
     <HeroLink key={heroItem.title}
       class={heroItem.class}
