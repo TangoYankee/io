8dc76ef9eaa7ba3b110c6ce238e4117af65e7722
Timothy Miller <miller.tim108@gmail.com>
2020-05-11 13:13:31 -0700

    Link transitions (#8)
    * undo react and jsk namespace and module
    
    * image cropped and border squared
    
    * header with internal links. contact information under picture
    
    * build
    
    * links swapped with span
    
    * general structure for state change
    
    * links stylized
    
    * links on on active
    
    * first react functionality: click for contact links
    
    * react build
    
    * build with red

--- src/components/Hero/HeroLink/index.tsx
+++ src/components/Hero/HeroLink/index.tsx
@@ -1,41 +1,44 @@
 import * as React from 'react'
 import 'components/Hero/HeroLink/index.css'
+import * as linkedInIcon from 'img/linkedin-logo.png';
+import * as githubIcon from 'img/GitHub-Mark-64px.png';
+import * as emailIcon from 'img/baseline_email_black_18dp.png';
 
 interface HeroLinkProps {
-  class: string
+  icon: string
   title: string
   href: string
-  span: string
+  display: string
 }
 
 export const heroLinks: HeroLinkProps[] = [
   {
-    class: 'linked-in-link',
+    icon: linkedInIcon,
     title: 'linkedin.com/in/timothy-m-miller/',
     href: 'https://www.linkedin.com/in/timothy-m-miller/',
-    span: 'linkedin at timothy-m-miller'
+    display: '/timothy-m-miller'
   },
   {
-    class: 'github-link',
+    icon: githubIcon,
     title: 'github.com/tangoyankee',
     href: 'https://github.com/tangoyankee',
-    span: 'github at tangoyankeer'
+    display: '@tangoyankee'
   },
   {
-    class: 'email-link',
+    icon: emailIcon,
     title: 'mailto:miller.tim108@gmail.com',
     href: 'mailto:miller.tim108@gmail.com',
-    span: 'miller.tim108@gmail.com'
+    display: 'miller.tim108@gmail.com'
   }
 ]
 
 export class HeroLink extends React.Component<HeroLinkProps, {}> {
   render () {
     return (
-      <a className={this.props.class} title={this.props.title}
-        href={this.props.href} target="_blank" rel="noopener noreferrer" >
-        <span>{this.props.span}</span>
-      </a>
+      <div className="contact-container">
+        <img className="contact-icon" src={this.props.icon} alt="icon"/>
+        <a href={this.props.href} title={this.props.title} target="_blank" rel="noopener noreferrer">{this.props.display}</a>
+      </div>
     )
   }
 }
